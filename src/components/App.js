d5a23939d7814e3a82ed36c9f2c5c532c4c94289
ismaelDembele <dembele.ismael@gmail.com>
2021-09-06 11:12:25 -0400

    suspense and react.lazy added

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import Header from "./Header/Header";
-import Footer from "./Footer";
-import LandingPage from "../pages/LandingPage";
-import Products from "../pages/Products";
-import SearchPage from "../pages/SearchPage";
-import ItemReview from "../pages/ItemReview";
-import ShoppingCart from "../pages/ShoppingCart";
-import SignIn from "../pages/SignIn";
-import Register from "../pages/Register";
-import Account from "../pages/Account";
-import { ProtectedRoute } from "./Protected";
 
 import CircularProgress from "@material-ui/core/CircularProgress";
 
 import { myContext } from "../Helper/context";
 import { useFetch } from "../Helper/useFetch";
 import axios from "axios";
+
+const Header = React.lazy(() => import("./Header/Header"));
+const Footer = React.lazy(() => import("./Footer"));
+const LandingPage = React.lazy(() => import("../pages/LandingPage"));
+const Products = React.lazy(() => import("../pages/Products"));
+const SearchPage = React.lazy(() => import("../pages/SearchPage"));
+const ItemReview = React.lazy(() => import("../pages/ItemReview"));
+const ShoppingCart = React.lazy(() => import("../pages/ShoppingCart"));
+const SignIn = React.lazy(() => import("../pages/SignIn"));
+const Register = React.lazy(() => import("../pages/Register"));
+const Account = React.lazy(() => import("../pages/Account"));
+const { ProtectedRoute } = React.lazy(() => import("../pages/Account"));
 
 axios.defaults.withCredentials = true;
 
@@ -42,56 +42,58 @@ const App = () => {
 
   return (
     <myContext.Provider value={{ myCart, setMyCart, myData, isLoggin, setIsLoggin, path }}>
-      <div className="layout">
-        <Router>
-          <Route
-            //the header is not display on the sign and register page
-            path="*"
-            render={props =>
-              props.location.pathname !== "/sign" &&
-              props.location.pathname !== "/register" && <Header />
-            }
-          />
-          {loading && <CircularProgress className="circular-progress" />}
-          <Switch>
-            <Route exact path="/">
-              <LandingPage />
-            </Route>
-            <Route exact path="/shopping">
-              <ShoppingCart />
-            </Route>
-            <Route exact path="/sign">
-              <SignIn />
-            </Route>
-            <Route exact path="/register">
-              <Register />
-            </Route>
-            <ProtectedRoute exact path="/account" component={Account} />
+      <Suspense fallback={<div>"...loading"</div>}>
+        <div className="layout">
+          <Router>
+            <Route
+              //the header is not display on the sign and register page
+              path="*"
+              render={props =>
+                props.location.pathname !== "/sign" &&
+                props.location.pathname !== "/register" && <Header />
+              }
+            />
+            {loading && <CircularProgress className="circular-progress" />}
+            <Switch>
+              <Route exact path="/">
+                <LandingPage />
+              </Route>
+              <Route exact path="/shopping">
+                <ShoppingCart />
+              </Route>
+              <Route exact path="/sign">
+                <SignIn />
+              </Route>
+              <Route exact path="/register">
+                <Register />
+              </Route>
+              <ProtectedRoute exact path="/account" component={Account} />
 
-            <Route exact path="/search/:id">
-              <SearchPage />
-            </Route>
-            <Route exact path="/products/:id">
-              <Products />
-            </Route>
-            <Route exact path="/itemreview/:id">
-              <ItemReview />
-            </Route>
+              <Route exact path="/search/:id">
+                <SearchPage />
+              </Route>
+              <Route exact path="/products/:id">
+                <Products />
+              </Route>
+              <Route exact path="/itemreview/:id">
+                <ItemReview />
+              </Route>
 
-            <Route exact path="*">
-              <div id="error-page">404 Page not found</div>
-            </Route>
-          </Switch>
+              <Route exact path="*">
+                <div id="error-page">404 Page not found</div>
+              </Route>
+            </Switch>
 
-          <Route
-            path="*"
-            render={props =>
-              props.location.pathname !== "/sign" &&
-              props.location.pathname !== "/register" && <Footer />
-            }
-          />
-        </Router>
-      </div>
+            <Route
+              path="*"
+              render={props =>
+                props.location.pathname !== "/sign" &&
+                props.location.pathname !== "/register" && <Footer />
+              }
+            />
+          </Router>
+        </div>
+      </Suspense>
     </myContext.Provider>
   );
 };
