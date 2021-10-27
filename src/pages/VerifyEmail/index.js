2c52aeb340fedddee1fc9e9d9d4968ea9489687f
ismaelDembele <dembele.ismael@gmail.com>
2021-10-27 14:44:58 -0400

    useEffect dependency warnings removed

--- src/pages/VerifyEmail/index.js
+++ src/pages/VerifyEmail/index.js
@@ -39,7 +39,7 @@ const Index = () => {
       .catch(error => {
         console.log("Could not send the token to the server:", error);
       });
-  }, []);
+  }, [id, path]);
 
   return (
     <div className="verifyAccount">
